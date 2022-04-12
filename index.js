4e74a4bff1116b93128a20e01686b0d4f84c2c09
闫韬蒙 <3094969938@qq.com>
2022-04-13 00:42:52 +0800

    修复了打包的问题

--- index.js
+++ index.js
@@ -2,9 +2,9 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import { AppRegistry } from 'react-native';
 import App from './App';
-import {name as appName} from './app.json';
+import { name as appName } from './app.json';
 
 import NavigationHelper from './src/utils/navigationHelper';
 import Http from './src/action/request';
@@ -13,3 +13,9 @@ NavigationHelper.init(NavigationHelper);
 Http.init(Http);
 
 AppRegistry.registerComponent(appName, () => App);
+
+//    <application>
+//     <meta-data
+//         android:name="com.amap.api.v2.apikey"
+//         android:value="c812f0c34d0f0d829e73859b16ee4a44" />
+//     </application>
